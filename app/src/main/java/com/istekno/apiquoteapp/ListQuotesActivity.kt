b6adf6e8e225501b566c9d243ff250b8eb51bb42
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2020-11-21 10:24:11 +0700

    Initial commit

---
+++ app/src/main/java/com/istekno/apiquoteapp/ListQuotesActivity.kt
@@ -0,0 +1,85 @@
+package com.istekno.apiquoteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_list_quotes.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
+
+class ListQuotesActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = ListQuotesActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_quotes)
+
+        supportActionBar?.title = "List of Quotes"
+
+        getListQuotes()
+    }
+
+    private fun getListQuotes() {
+        progressBarLQ.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/list"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray
+            ) {
+                progressBarLQ.visibility = View.INVISIBLE
+
+                val listQuotes = ArrayList<String>()
+                val result = String(responseBody)
+                Log.d(TAG, result)
+
+                try {
+                    val jsonArray = JSONArray(result)
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val quote = jsonObject.getString("en")
+                        val author = jsonObject.getString("author")
+                        listQuotes.add("\n$quote\n\n -- $author\n")
+                    }
+
+                    val adapter = ArrayAdapter(this@ListQuotesActivity, android.R.layout.simple_list_item_1, listQuotes)
+                    listQuotesLV.adapter = adapter
+                } catch (e: Exception) {
+                    Toast.makeText(this@ListQuotesActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                progressBar.visibility = View.INVISIBLE
+
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+
+                Toast.makeText(this@ListQuotesActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/istekno/apiquoteapp/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.istekno.apiquoteapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getRandomQuote()
+
+        btnAllQuotes.setOnClickListener {
+            val intent = Intent(this, ListQuotesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getRandomQuote() {
+        progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/list"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray
+            ) {
+                progressBar.visibility = View.INVISIBLE
+
+                val result = String(responseBody)
+                Log.d(TAG, result)
+
+                try {
+                    val jsonArray = JSONArray(result)
+                    val responsObject = jsonArray.getJSONObject(0)
+
+                    val quote = responsObject.getString("en")
+                    val author = responsObject.getString("author")
+
+                    tvQuote.text = quote
+                    tvAuthor.text = author
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                progressBar.visibility = View.INVISIBLE
+
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
